d965b4675c023e10a24341dfeb8dab3ad12f2efc
someshnarwade <someshnarwade@gmail.com>
2019-11-13 19:43:41 +0530

    Added ATOS-SYNTEL Pyramid Output

--- Pyramid/src/myPyramid.java
+++ Pyramid/src/myPyramid.java
@@ -2,8 +2,26 @@
 public class myPyramid {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello Pyramid!");
+		String givenString = "ATOS-SYNTEL";
+		System.out.println(givenString);
+		int n = 0, m =0;
+		
+		while(n <= givenString.length()) {
+			if(n <= givenString.length()) {
+				
+				while(m < n) {
+					System.out.print(" ");
+					System.out.print(givenString.charAt(m));
+					System.out.print(" ");
+					m++;
+				}
+						
+			}
+			System.out.println();
+			m=0;
+			n++;
+		}
+		System.out.println("Out of while loop");
 	}
 
 }
