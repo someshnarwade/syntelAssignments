2fdecab67278435eb7b6f8ce0ccdae670607817c
crushmaster47 <someshnarwade9@gmail.com>
2019-11-18 18:58:52 +0530

    Change is good

--- Calculator/src/myCalculator.java
+++ Calculator/src/myCalculator.java
@@ -4,6 +4,7 @@ public class myCalculator {
 	public static void main(String[] args) {
 		System.out.println("Hello Calculator!");
 		System.out.println("Hello Again!");
+		System.out.println("Change is good!");
 	}
 
 }
