d282c2402b63a85287492e6dc75c5d3c954f094b
kf <kf@221b>
2019-11-18 17:52:10 +0530

    Completed the assignment. Code needs comments.

--- Pyramid/src/myPyramid.java
+++ Pyramid/src/myPyramid.java
@@ -2,26 +2,40 @@
 public class myPyramid {
 
 	public static void main(String[] args) {
-		String givenString = "ATOS-SYNTEL";
-		System.out.println(givenString);
-		int n = 0, m =0;
 		
-		while(n <= givenString.length()) {
-			if(n <= givenString.length()) {
-				
-				while(m < n) {
-					System.out.print(" ");
-					System.out.print(givenString.charAt(m));
-					System.out.print(" ");
-					m++;
-				}
+		String givenString = "ATOS-SYNTEL";
+		int LENGTH = givenString.length() + 1;
+		int n = 1, m = 0,x = 0;
+		
+		while(n <= LENGTH) {
+			
+			if(n <= LENGTH) {
 						
+				if(n == LENGTH) {
+					
+				}
+				else {
+					
+					while(x < (LENGTH - n)) {
+						System.out.print(" ");
+						x++;
+						
+					}
+					
+					while(m < (LENGTH - x)) {
+						System.out.print(givenString.charAt(m));
+						System.out.print(" ");
+						m++;
+					}
+
+				}
+				m=0;	
+				
 			}
 			System.out.println();
-			m=0;
+			
+			x=0;
 			n++;
 		}
-		System.out.println("Out of while loop");
 	}
-
 }
